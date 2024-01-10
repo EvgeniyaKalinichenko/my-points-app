2eec87a6214a85c26e3e920a9dbdf01d7fc9637e
svetk0 <sv.solomatnikova@gmail.com>
2024-01-11 01:23:00 +0300

    working on data and hours

--- src/scripts/cityApi/cityCurrency.js
+++ src/scripts/cityApi/cityCurrency.js
@@ -5,6 +5,7 @@
 const submitBtn = document.querySelector('.submitBtn');
 const cityName = document.querySelector('.card-name');
 const cityCurrency = document.querySelector('.card-currency');
+const cityTimezone = document.querySelector('.card-timezone');
 // const formContainer = document.querySelector('.form_container');
 // const countryInfoContainer = document.querySelector('.countryInfoContainer');
 
@@ -28,8 +29,22 @@ function printCurrency(currency) {
 function printCityName(city) { 
     //console.log('cityName:');
     cityName.textContent = city;
+}
+function printTimeZone(timezone) { 
+    cityTimezone.textContent = 'Time zone: '+timezone;
+}
+
+function getLocalTime(localTimezone) { 
+    let date = new Date().getTime();
+    console.log('current time: '+date);
+    //console.log(`Current date: ${date.getHours()}:${date.getMinutes()}`);
+    let localTime = 0;
+    let myTime = new Date();
+
 
 }
+getLocalTime();
+
 async function getCountryData(capitalCity) {
     const countryResource = await fetch('https://restcountries.com/v3.1/capital/' + capitalCity);
     const data = await countryResource.json()
@@ -37,6 +52,7 @@ async function getCountryData(capitalCity) {
     //printCurrency(data.currencies[Object.keys(data[0].currencies)[0]].name);
     printCurrency((data[0].currencies[Object.keys(data[0].currencies)[0]].name) + ' (' + (data[0].currencies[Object.keys(data[0].currencies)[0]].symbol) + ')');
     printCityName(data[0].capital);
+    printTimeZone(data[0].timezones);
 
     countryNames = data.map((country) => {
         return country.name.common;
